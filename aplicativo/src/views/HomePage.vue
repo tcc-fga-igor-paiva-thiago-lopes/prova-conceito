5fd0d79cadcb02072f04171a4d55eea76de18907
igor-paiva <igorbatistapaiva@outlook.com>
2023-01-26 01:31:18 -0300

    Fetch API trips in home page

--- aplicativo/src/views/HomePage.vue
+++ aplicativo/src/views/HomePage.vue
@@ -2,48 +2,69 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Inbox</ion-title>
+        <ion-title>Viagens</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
+      <!-- <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
         <ion-refresher-content></ion-refresher-content>
-      </ion-refresher>
+      </ion-refresher> -->
 
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Inbox</ion-title>
+          <ion-title size="large">Viagens</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <ion-list>
-        <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
+        <!-- <MessageListItem v-for="message in messages" :key="message.id" :message="message" /> -->
+        <p v-for="trip in trips" :key="trip.id">
+          {{trip.text}}
+        </p>
       </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import {
   IonContent,
   IonHeader,
   IonList,
   IonPage,
-  IonRefresher,
-  IonRefresherContent,
+  // IonRefresher,
+  // IonRefresherContent,
   IonTitle,
   IonToolbar,
 } from '@ionic/vue';
-import MessageListItem from '@/components/MessageListItem.vue';
-import { getMessages, Message } from '@/data/messages';
-import { ref } from 'vue';
 
-const messages = ref<Message[]>(getMessages());
+export default {
+  data(): {
+    trips: {id: number, text: string}[],
+  } {
+    return {
+      trips: [],
+    };
+  },
+  created() {
+    const apiBaseUrl = process.env.VUE_APP_API_URL || 'http://localhost:5001';
 
-const refresh = (ev: CustomEvent) => {
-  setTimeout(() => {
-    ev.detail.complete();
-  }, 3000);
+    fetch(`${apiBaseUrl}/trips`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        this.trips = data.list;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  mounted() {
+    console.log('trips: ', this.trips);
+  },
+  methods: {
+  }
 };
 </script>
