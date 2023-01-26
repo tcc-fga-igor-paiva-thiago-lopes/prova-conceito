c94eb2769a19566537fc849e71b375bb77af5fbf
igor-paiva <igorbatistapaiva@outlook.com>
2023-01-26 15:11:21 -0300

    Change screen to trips

--- aplicativo/src/views/HomePage.vue
+++ aplicativo/src/views/HomePage.vue
@@ -7,64 +7,117 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <!-- <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
-        <ion-refresher-content></ion-refresher-content>
-      </ion-refresher> -->
-
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Viagens</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ion-list>
-        <!-- <MessageListItem v-for="message in messages" :key="message.id" :message="message" /> -->
-        <p v-for="trip in trips" :key="trip.id">
-          {{trip.text}}
-        </p>
-      </ion-list>
+      <div v-if="loading">
+        <IonLoading />
+      </div>
+
+      <div v-if="!loading">
+        <IonCard v-for="trip in trips" :key="trip.id">
+          <IonCardHeader class="trip-card-header">
+            {{trip.description}}
+          </IonCardHeader>
+
+          <IonCardContent>
+            <div class="pair-trip-container">
+              <span><strong>Origem: </strong> {{ trip.origin }}</span>
+              <span><strong>Destino: </strong> {{ trip.destination }}</span>
+            </div>
+
+            <div class="pair-trip-container" style="margin-top: 16px">
+              <span><strong>Distância: </strong> {{ `${trip.distance} km` }}</span>
+              <span><strong>Tipo de carga: </strong> {{ trip.cargo_type }}</span>
+            </div>
+
+            <div class="pair-trip-container" style="margin-top: 16px">
+              <span><strong>Peso carga: </strong> {{ `${trip.cargo_weight} kg` }}</span>
+              <span><strong>Contratante: </strong> {{ trip.company }}</span>
+            </div>
+
+            <div class="pair-trip-container" style="margin-top: 16px">
+              <span><strong>Início: </strong> {{ trip.start_date }}</span>
+              <span><strong>Fim: </strong> {{ trip.end_date }}</span>
+            </div>
+          </IonCardContent>
+        </IonCard>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<!-- lang="ts" -->
+<script>
 import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
   IonContent,
   IonHeader,
-  IonList,
+  IonLoading,
   IonPage,
-  // IonRefresher,
-  // IonRefresherContent,
   IonTitle,
   IonToolbar,
 } from '@ionic/vue';
 
+// interface Trip {
+//   id: number;
+//   origin: string;
+//   destination: string;
+//   distance: number;
+//   cargo_type: string;
+//   cargo_weight: number;
+//   start_date: string;
+//   end_date: string;
+//   description: string;
+//   company: string;
+// }
+
 export default {
-  data(): {
-    trips: {id: number, text: string}[],
-  } {
+  data() {
     return {
-      trips: [],
+      loading: false,
+      trips: [], // as Trip[],
     };
   },
   created() {
     const apiBaseUrl = process.env.VUE_APP_API_URL || 'http://localhost:5001';
+    // const apiBaseUrl = 'http://localhost:5001';
+
+    this.loading = true;
 
     fetch(`${apiBaseUrl}/trips`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-
-        this.trips = data.list;
+        console.log('request data: ', data);
+        console.log('request data list: ', data.list);
+        this.trips = [...data.list]; // as Trip[];
       })
       .catch((error) => {
         console.log(error);
+      })
+      .finally(() => {
+        this.loading = false;
       });
   },
-  mounted() {
-    console.log('trips: ', this.trips);
-  },
-  methods: {
-  }
+  components: { IonLoading, IonCard, IonCardHeader, IonCardContent }
 };
 </script>
+
+<style scoped>
+.pair-trip-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.trip-card-header {
+  margin: 0 auto;
+  font-size: 20px;
+  font-weight: bold;
+  width: fit-content;
+}
+</style>
