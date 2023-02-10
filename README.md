df8b29d541d1706dff0d7c7baa59779912f45c71
igor-paiva <igorbatistapaiva@outlook.com>
2023-02-10 13:11:04 -0300

    Deploy API with new changes

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Simulaçao Arquitetural
+# Prova de Conceito TCC1 
 
-Simulação arquitetural elaborada para a 1ª etapa do Trabalho de Conclusão de Curso de Engenharia de Software, na Universidade de Brasília (UnB).
-
-## Autores
+Autores:
 
 - Igor Batista Paiva
 - Thiago Lopes Santos
--- server.py
+++ server.py
@@ -1,6 +1,27 @@
-from src.app import create_app
-
-app, _, _ = create_app()
+from src.app import app, db
+from flask.cli import FlaskGroup
+from src.model.truck_driver import TruckDriver
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+cli = FlaskGroup(app)
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(
+        TruckDriver(
+            name="John",
+            email="john@mail.com",
+            password="password123",
+            password_confirmation="password123"
+        )
+    )
+    db.session.commit()
