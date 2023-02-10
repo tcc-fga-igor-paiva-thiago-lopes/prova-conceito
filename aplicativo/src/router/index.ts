4a3e179905f4d5b702456ca31fe9ea675216a9c6
igor-paiva <igorbatistapaiva@outlook.com>
2023-02-10 12:11:13 -0300

    Creates signup screen

--- aplicativo/src/router/index.ts
+++ aplicativo/src/router/index.ts
@@ -1,16 +1,22 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import SignUp from '../views/SignUp.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/signup'
   },
   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
   },
 ]
 
